8f808a4e14dd0d1e11f8feaa5ac7d689565f15e8
Hamish B <hmblair@stanford.edu>
2023-12-02 17:43:18 -0800

    Removed default device of cpu

--- tests.sh
+++ tests.sh
@@ -3,12 +3,11 @@
 python3 test.py \
     --model_folder lightning_logs \
     --batch_size 16 \
-    --way 5 \
+    --way 2 \
     --shot 2 \
     --dataset imagenet-tiny \
     --num_workers 0 \
-    --accelerator cpu \
-
+\
 
 python3 test.py \
     --model_folder lightning_logs \
@@ -17,4 +16,3 @@ python3 test.py \
     --shot 5 \
     --dataset imagenet-tiny \
     --num_workers 0 \
-    --accelerator cpu \
