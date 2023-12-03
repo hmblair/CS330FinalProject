4f511832bcf70915893f41aa60499abc06c87c59
Hamish B <hmblair@stanford.edu>
2023-12-03 14:21:20 -0800

    Added an array of tests

--- tests.sh
+++ tests.sh
@@ -1,18 +1,186 @@
 #!/bin/bash
 
+
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 2 \
+    --shot 1 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 5 \
+    --shot 1 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 10 \
+    --shot 1 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 25 \
+    --shot 1 \
+    --dataset $1 \
+    --num_workers 1 \
+
+
+
+
+
+
+
 python3 test.py \
     --model_folder lightning_logs \
     --batch_size 16 \
     --way 2 \
     --shot 2 \
-    --dataset imagenet-tiny \
-    --num_workers 0 \
-\
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 5 \
+    --shot 2 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 10 \
+    --shot 2 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 25 \
+    --shot 2 \
+    --dataset $1 \
+    --num_workers 1 \
+
+
+
+
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 2 \
+    --shot 5 \
+    --dataset $1 \
+    --num_workers 1 \
 
 python3 test.py \
     --model_folder lightning_logs \
     --batch_size 16 \
     --way 5 \
     --shot 5 \
-    --dataset imagenet-tiny \
-    --num_workers 0 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 10 \
+    --shot 5 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 25 \
+    --shot 5 \
+    --dataset $1 \
+    --num_workers 1 \
+
+
+
+
+
+
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 2 \
+    --shot 10 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 5 \
+    --shot 10 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 10 \
+    --shot 10 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 25 \
+    --shot 10 \
+    --dataset $1 \
+    --num_workers 1 \
+
+
+
+
+
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 2 \
+    --shot 25 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 5 \
+    --shot 25 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 10 \
+    --shot 25 \
+    --dataset $1 \
+    --num_workers 1 \
+
+python3 test.py \
+    --model_folder lightning_logs \
+    --batch_size 16 \
+    --way 25 \
+    --shot 25 \
+    --dataset $1 \
+    --num_workers 1 \
+
+
