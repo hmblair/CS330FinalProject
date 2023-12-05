9f2c02c6865e37f7ab06ac62bd30b5b3d06e8fad
Hamish B <hmblair@stanford.edu>
2023-12-04 16:47:44 -0800

    Added better logging capabilities

--- test.py
+++ test.py
@@ -6,19 +6,21 @@ from protonet import ProtoNetICL, ProtoNetSkip, ProtoNetWithoutEncoder
 from Data import ClipDataModule
 import os
 import warnings
+from itertools import product
 
 if __name__ == '__main__':
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
     parser = argparse.ArgumentParser(prog='Scalable In-Context Meta-Learning')
     parser.add_argument('--model_folder', type=str)
-    parser.add_argument('--way', type=int)
-    parser.add_argument('--shot', type=int)
+    parser.add_argument('--way', type=int, nargs='+')
+    parser.add_argument('--shot', type=int, nargs='+')
     parser.add_argument('--dataset', type=str)
     parser.add_argument('--batch_size', type=int)
     parser.add_argument('--accelerator', type=str, default='gpu')
     parser.add_argument('--num_workers', type=int, default=0)
-    parser.add_argument('--clip_only', action='store_true')
     args = parser.parse_args()
-
 
     # get the data path
     if args.dataset == 'imagenet-tiny':
@@ -34,33 +36,41 @@ if __name__ == '__main__':
         download_decathalon()
         data_path = os.path.join('Data', 'decathlon')
         paths = {'test' : data_path}
+    elif args.dataset == 'indoor_scenes':
+        from downloader import download_indoor_scenes
+        download_indoor_scenes()
+        data_path = os.path.join('Data', 'indoor_scenes')
+        paths = {'test' : data_path}
     else: 
         raise ValueError(f'Invalid dataset name {args.dataset}')
-
-    # initialise the data module
-    datamodule = ClipDataModule(
-        paths = paths,
-        batch_size = args.batch_size,
-        way = args.way,
-        shot = args.shot,
-        num_workers = args.num_workers,
-        )
     
-    # test the models in the given folder
-    if not args.clip_only:
+
+    for way, shot in product(args.way, args.shot):
+        # initialise the data module
+        datamodule = ClipDataModule(
+            paths = paths,
+            batch_size = args.batch_size,
+            way = way,
+            shot = shot,
+            num_workers = args.num_workers,
+            )
+        
+
+        import pandas as pd
+        data = pd.DataFrame(columns=['model', 'way', 'shot', 'test_accuracy'])
+        
+        # test the models in the given folder
         for dir in os.listdir(args.model_folder):
             if os.path.isfile(dir):
                 continue
             model_name = dir.split('_')[0]
-            ckpt_dir = os.path.join(args.model_folder, dir, 'checkpoints', 'best.ckpt')
+            ckpt_dir = os.path.join(args.model_folder, dir, 'checkpoints', 'last.ckpt')
 
             # initialise the trainer
-            log_dir = 'test_logs'
-            logger = CSVLogger(log_dir, name=dir, version=0)
             trainer = pl.Trainer(
                 accelerator = args.accelerator,
                 precision = '16-mixed' if args.accelerator == 'gpu' else '32',
-                logger = logger,
+                logger = None,
                 )
 
             # initialise the model
@@ -75,18 +85,30 @@ if __name__ == '__main__':
             model = model.load_from_checkpoint(ckpt_dir)
 
             # test the model
-            print(f'Testing {dir} on {args.dataset} with {args.way}-way {args.shot}-shot')
-            trainer.test(model, datamodule)
+            print(f'Testing {dir} on {args.dataset} with {way}-way {shot}-shot')
 
-    # test ProtoNetWithoutEncoder
-    log_dir = 'test_logs'
-    logger = CSVLogger(log_dir, name='ProtoNetWithoutEncoder', version=0)
-    trainer = pl.Trainer(
-        accelerator = args.accelerator,
-        precision = '16-mixed' if args.accelerator == 'gpu' else '32',
-        logger = logger,
+            df2 = pd.DataFrame(
+                data = [[dir, way, shot, trainer.test(model, datamodule)[0]['test_accuracy_epoch']]],
+                columns = ['model', 'way', 'shot', 'test_accuracy']
+            )
+            data = pd.concat([data, df2], ignore_index=True)
+
+
+        # test ProtoNetWithoutEncoder
+        trainer = pl.Trainer(
+            accelerator = args.accelerator,
+            precision = '16-mixed' if args.accelerator == 'gpu' else '32',
+            logger = None,
+            )
+        
+        print(f'Testing ProtoNetWithoutEncoder on {args.dataset} with {args.way}-way {args.shot}-shot')
+        model = ProtoNetWithoutEncoder()
+        df2 = pd.DataFrame(
+            data = [['ProtoNetWithoutEncoder', way, shot, trainer.test(model, datamodule)[0]['test_accuracy_epoch']]],
+            columns = ['model', 'way', 'shot', 'test_accuracy']
         )
-    
-    print(f'Testing ProtoNetWithoutEncoder on {args.dataset} with {args.way}-way {args.shot}-shot')
-    model = ProtoNetWithoutEncoder()
-    trainer.test(model, datamodule)
+        data = pd.concat([data, df2], ignore_index=True)
+
+        print(data)
+        data.to_csv('results.csv')
+
